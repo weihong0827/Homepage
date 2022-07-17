84ef97cd2f9b925459b66bf0d9a45f032441c2de
weihong0827 <48542562+weihong0827@users.noreply.github.com>
2022-07-17 11:50:06 +0800

    Initial setup with project and post page ready

---
+++ pages/projects.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { domain } from 'lib/config'
+import { resolveNotionPage } from 'lib/resolve-notion-page'
+import { NotionPage } from 'components'
+
+export const getStaticProps = async () => {
+  const projectPageId = '632735c8655b4a81a396f03748adf486'
+  try {
+    const props = await resolveNotionPage(domain, projectPageId)
+
+    return { props, revalidate: 10 }
+  } catch (err) {
+    console.error('page error', domain, err)
+
+    // we don't want to publish the error version of this page, so
+    // let next.js know explicitly that incremental SSG failed
+    throw err
+  }
+}
+
+export default function NotionDomainPage(props) {
+  return <NotionPage {...props} />
+}
